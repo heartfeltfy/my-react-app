6dbcea4e5f5c9220dba8fafaccb64b62e81c9fa0
chenmf <15239905718@163.com>
2023-11-27 14:55:13 +0800

    refactor：测试打包预览

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,7 @@
-import AuthProvider from "@/layout/AuthProvider.tsx"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import PublicPage from "@/pages/PublicPage.tsx"
-import ProtectedPage from "@/pages/ProtectedPage.tsx"
-import RequireAuth from "@/layout/RequireAuth.tsx"
-import Root from "@/routes/Root.tsx"
-import LoginPage from "@/pages/login/LoginPage.tsx"
-import ErrorPage from "@/pages/ErrorPage.tsx"
-import { useCopy } from "@/hooks/useCopy.ts"
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    errorElement: <ErrorPage />,
-    element: (
-      <AuthProvider>
-        <Root />
-      </AuthProvider>
-    ),
-    children: [
-      {
-        index: true,
-        element: <PublicPage />
-      },
-      {
-        path: "/protected",
-        element: (
-          <RequireAuth>
-            <ProtectedPage />
-          </RequireAuth>
-        )
-      }
-    ]
-  },
-  {
-    path: "/login",
-    element: <LoginPage />
-  }
-])
 export default function App() {
-  useCopy()
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <div>哈哈这是测试打包</div>
+    </>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,19 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/my-react-app/",
-  base: "./",
+  base: "/my-react-app/",
+  // base: "./",
   plugins: [react()],
   build: {
     outDir: "docs",
     // 设置最终构建的浏览器兼容目标
     target: "es2015",
     // 构建后是否生成 source map 文件
-    sourcemap: false
+    sourcemap: false,
     //  chunk 大小警告的限制（以 kbs 为单位）
-    // chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 2000,
     // 启用/禁用 gzip 压缩大小报告
-    // reportCompressedSize: false
+    reportCompressedSize: false
   },
   resolve: {
     alias: {
